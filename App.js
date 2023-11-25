14c605e113abbe4e767e9e2cdce9907d428c0a91
Soyeon Kim <kksoyeon61@gmail.com>
2023-11-25 14:36:08 +0900

    Create App.js

---
+++ App.js
@@ -0,0 +1,142 @@
+import './App.css';
+import React, { useContext, useReducer,useRef } from 'react';
+import {BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
+
+import DiaryList from './Components/DiaryList';
+
+
+
+
+const reducer = (state,action) => {
+  let newState = [];
+  switch(action.type){
+    case 'INIT':{
+      return action.data;
+    }
+    case 'CREATE':{
+      const newState = [action.data, ...state];
+    }
+    case 'REMOVE':{
+      newState = state.filter((it)=>it.id !== action.targetId);
+      break;
+    }
+    case 'EDIT' :{
+      newState = state.map((it)=>
+      it.id === action.data.id ? { ...action.data}: it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  
+  return newState;
+}
+
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+const dummyData = [
+  {
+    id:1,
+    emotion: 1,
+    content: "오늘의 일기 1번",
+    date : 1700728514048,
+  },
+  {
+    id:2,
+    emotion: 2,
+    content: "오늘의 일기 2번",
+    date : 1700728514049,
+  },
+  {
+    id:3,
+    emotion: 3,
+    content: "오늘의 일기 3번",
+    date : 1700728514050,
+  },
+  {
+    id:4,
+    emotion: 4,
+    content: "오늘의 일기 4번",
+    date : 170072851404051,
+  },
+  {
+    id:5,
+    emotion: 5,
+    content: "오늘의 일기 5번",
+    date : 1700728514052,
+  },
+ 
+];
+
+
+
+function App() {
+
+  const [data,dispatch] = useReducer(reducer,[]);
+  
+  console.log(new Date().getTime());
+  const dataId = useRef(0);
+  // CREATE
+
+  const onCreate = (date,content,emotion) => {
+    dispatch({
+      type:"CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      }
+    });
+    dataId.current += 1;
+  }
+  // REMOVE
+  const onRemove = (targetId)=>{
+    dispatch({type: "REMOVE",targetId});
+  };
+  // EDIT
+  const onEdit = (targetId, date,content,emotion)=>{
+    dispatch({
+      type:"EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{
+        onCreate,
+        onEdit,
+        onRemove,
+      }}>
+    <BrowserRouter>
+    <div className="App">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/edit" element={<Edit />} />
+      <Route path="/diary" element={<Diary />} />
+
+    </Routes>
+  
+    
+    </div>
+    </BrowserRouter>
+    </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
+  );
+}
+
+export default App;
